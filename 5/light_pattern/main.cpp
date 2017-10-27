eb3d40e788f41e7f25a2655362227622b573612a
a-enz <andi_enz@msn.com>
2017-10-27 20:24:41 +0200

    Slow implementation of evolution

--- 5/light_pattern/main.cpp
+++ 5/light_pattern/main.cpp
@@ -6,12 +6,29 @@
 using namespace std;
 
 void testcase() {
-	int n_words;
-	cin >> n_words;
+	int n_lights, k, exp_pattern;
+	cin >> n_lights >> k >> exp_pattern;
+
+	vector<int> light_pattern(n_lights/k);
+	cout << "exp_pattern " << exp_pattern << endl;
+	for(int i=0; i<n_lights/k; i++) {
+		int pattern=0;
+		for(int j=k-1; j>=0; j--) {
+			int state;
+			cin >> state;
+
+			pattern += state << j;
+		}
+
+		cout << pattern << " ";
+	}
+
+	cout << endl;
+
+
 
 
 	//write output values for testcase
-		cout << sum << '\n';
 }
 
 int main(void) {
