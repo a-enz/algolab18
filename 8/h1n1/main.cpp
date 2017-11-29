63f363e99ec1fdac090eeedf40688d2d6abff567
a-enz <andi_enz@msn.com>
2017-11-29 15:16:48 +0100

    h1n1 finished

---
+++ 8/h1n1/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <unordered_set>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+using namespace std;
+
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
+typedef Triangulation::Finite_faces_iterator  Face_iterator;
+typedef Triangulation::Finite_edges_iterator  Edge_iterator;
+typedef Triangulation::Face_handle  Face_handle;
+typedef K::Point_2  P;
+typedef Triangulation::Vertex_handle    Vertex_handle;
+
+
+bool finds_way_out(const Triangulation &t, const Face_handle &f, 
+                const double d, unordered_set<Face_handle> &visited) {
+    // check if we reached infinite face 
+    if(t.is_infinite(f) || f == NULL) 
+        return true;
+
+    visited.insert(f);
+    // iterate over edges of face and check distance condition
+    // if distance condition holds: 
+    // open new recursion for every neighboring face no yet visited
+
+
+    for (int i = 0; i < 3; ++i)
+    {
+        P p1 = f->vertex(i)->point();
+        P p2 = f->vertex((i+1) % 3)->point();
+        K::FT dist = CGAL::squared_distance(p1, p2);
+
+/*        cout << "distance between (" << p1 << "), (" << p2 <<
+            ") is " << dist << " and d=" << d << endl;
+*/
+        Face_handle nf = f->neighbor((i+2) % 3);
+
+        //check distance
+        //check nf not yet visited
+        if(4*d <= dist && 
+            visited.find(nf) == visited.end()) {
+
+            if(finds_way_out(t, nf, d, visited)) {
+                return true;
+            }
+        }
+    }
+
+    //only reached if no recursion returns 'true'
+    return false;
+}
+
+
+bool can_escape(const Triangulation &t, const P& start, const double d) {
+    // first check how far the start is from the nearest infected
+    Vertex_handle nearest = t.nearest_vertex(start);
+    double dist_nearest = CGAL::squared_distance(nearest->point(), start);
+
+
+    if(dist_nearest < d) {
+        return false;
+    }
+    else {
+        // check if there is an escape route
+        // do a bfs or dfs over all edges and try to get to
+        // the 'outside' by reaching infinite face or something
+        Face_handle f = t.locate(start, t.incident_faces(nearest)); 
+
+        unordered_set<Face_handle> visited;
+
+        return finds_way_out(t, f, d, visited);
+    }
+}
+
+void testcase(int n_infected) {
+
+    //read infected locations
+    vector<P> pts(n_infected);
+    for (int i = 0; i < n_infected; ++i)
+    {
+        cin >> pts[i]; 
+    }
+
+    // read query locations and radii
+    int m_queries;
+    cin >> m_queries;
+    vector<P> query_loc(m_queries);
+    vector<double> query_radius(m_queries);
+
+    for (int i = 0; i < m_queries; ++i)
+    {
+        cin >> query_loc[i];
+        cin >> query_radius[i];
+    }
+
+    // Triangulation of infected:
+    Triangulation t;
+    t.insert(pts.begin(), pts.end());
+
+    // process queries:
+    for(int i=0; i<m_queries; i++)
+    {
+        if(can_escape(t, query_loc[i], query_radius[i])) {
+            cout << "y";
+        }
+        else {
+            cout << "n";
+        }
+    }
+
+    cout << endl;
+}
+
+int main(void) {
+
+    //disable for performance
+    ios_base::sync_with_stdio(false);
+    //read total number of test cases
+    int n;
+    cin >> n;
+
+    //do for each testcase
+    while(n != 0){
+        testcase(n);
+        cin >> n;
+    }
+
+    return 0;
+}
