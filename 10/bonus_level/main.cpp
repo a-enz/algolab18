4244622a6294176681744605a5029ac944255ec2
a-enz <andi_enz@msn.com>
2018-01-05 17:53:48 +0100

    Ingenious grandmaster solution

--- 10/bonus_level/main.cpp
+++ 10/bonus_level/main.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <climits>
 #include <algorithm>
 
 using namespace std;
 
-int max_path
+int n;
+vector< vector<int> > coin;
+vector< vector< vector<int> > > memo;
+
+int best_paths(int d, int x1, int x2) {
+    int y1 = d - x1;
+    int y2 = d - x2;
+
+    //if we went outside of the level, return a very low value to punish
+    if(x1 == n || x2 == n || y1 == n || y2 == n) return INT_MIN;
+
+    //if precomputed result exists, return
+    if(memo[d][x1][x2] != -1) return memo[d][x1][x2];
+
+    //otherwise compute result recursively
+    int result;
+    if(x1 == x2) {
+        if(x1 == n-1) return coin[n-1][n-1];
+        else {
+            result =  coin[x1][y1] + 
+                max(best_paths(d+1, x1+1, x2), best_paths(d+1, x1, x2+1));
+        }
+    } 
+    else {
+        result =  coin[x1][y1] + coin[x2][y2] + max({
+                best_paths(d+1, x1+1, x2),
+                best_paths(d+1, x1, x2),
+                best_paths(d+1, x1, x2+1),
+                best_paths(d+1, x1+1, x2+1)
+            });
+
+    }
+
+    memo[d][x1][x2] = result;
+    return result;
+}
 
 void testcase() {
-	int n;
 	cin >> n;
 
-    vector< vector<int> > coins(n, vector<int>(n));
+    memo = vector< vector< vector<int> > >(2*n, vector< vector<int> >(n, vector<int>(n, -1)));
+
+    coin = vector< vector<int> > (n, vector<int>(n));
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < n; ++j)
@@ -20,7 +57,7 @@ void testcase() {
         }
     }
 
-
+    cout << best_paths(0, 0, 0) << endl;
     	
 }
 
