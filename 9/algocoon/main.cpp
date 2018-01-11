3b8809c477f93185f5f5bdfb70dd904413b42278
aenz <aenz@student.ethz.ch>
2018-01-11 14:32:08 +0100

    Better solution by someone not dropped on his head at birth

--- 9/algocoon/main.cpp
+++ 9/algocoon/main.cpp
@@ -8,55 +8,52 @@
 #include <queue>
 
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
-#include <boost/graph/find_flow_cost.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
 
 // Namespaces
 using namespace std;
 
 
 // BGL Graph definitions
-// ===================== 
-// Graph Type with nested interior edge properties for Cost Flow Algorithms
-typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
+// =====================
+// Graph Type with nested interior edge properties for Flow Algorithms
+typedef	boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
-    boost::property<boost::edge_capacity_t, long,
-        boost::property<boost::edge_residual_capacity_t, long,
-            boost::property<boost::edge_reverse_t, Traits::edge_descriptor,
-                boost::property <boost::edge_weight_t, long> > > > > Graph; // new!
+	boost::property<boost::edge_capacity_t, long,
+		boost::property<boost::edge_residual_capacity_t, long,
+			boost::property<boost::edge_reverse_t, Traits::edge_descriptor> > > >	Graph;
 // Interior Property Maps
-typedef boost::property_map<Graph, boost::edge_capacity_t>::type      EdgeCapacityMap;
-typedef boost::property_map<Graph, boost::edge_weight_t >::type       EdgeWeightMap; // new!
-typedef boost::property_map<Graph, boost::edge_residual_capacity_t>::type ResidualCapacityMap;
-typedef boost::property_map<Graph, boost::edge_reverse_t>::type       ReverseEdgeMap;
-typedef boost::graph_traits<Graph>::vertex_descriptor          Vertex;
-typedef boost::graph_traits<Graph>::edge_descriptor            Edge;
-typedef boost::graph_traits<Graph>::out_edge_iterator  OutEdgeIt; // Iterator
+typedef	boost::property_map<Graph, boost::edge_capacity_t>::type		EdgeCapacityMap;
+typedef	boost::property_map<Graph, boost::edge_residual_capacity_t>::type	ResidualCapacityMap;
+typedef	boost::property_map<Graph, boost::edge_reverse_t>::type		ReverseEdgeMap;
+typedef	boost::graph_traits<Graph>::vertex_descriptor			Vertex;
+typedef	boost::graph_traits<Graph>::edge_descriptor			Edge;
+typedef	boost::graph_traits<Graph>::out_edge_iterator			OutEdgeIt;
 
 // Custom Edge Adder Class, that holds the references
-// to the graph, capacity map, weight map and reverse edge map
-// ===============================================================
+// to the graph, capacity map and reverse edge map
+// ===================================================
 class EdgeAdder {
-    Graph &G;
-    EdgeCapacityMap &capacitymap;
-    EdgeWeightMap &weightmap;
-    ReverseEdgeMap  &revedgemap;
+	Graph &G;
+	EdgeCapacityMap	&capacitymap;
+	ReverseEdgeMap	&revedgemap;
 
 public:
-    EdgeAdder(Graph &G, EdgeCapacityMap &capacitymap, EdgeWeightMap &weightmap, ReverseEdgeMap &revedgemap) 
-        : G(G), capacitymap(capacitymap), weightmap(weightmap), revedgemap(revedgemap) {}
+	// to initialize the Object
+	EdgeAdder(Graph & G, EdgeCapacityMap &capacitymap, ReverseEdgeMap &revedgemap):
+		G(G), capacitymap(capacitymap), revedgemap(revedgemap){}
 
-    void addEdge(int u, int v, long c, long w) {
-        Edge e, rev_e;
-        boost::tie(e, boost::tuples::ignore) = boost::add_edge(u, v, G);
-        boost::tie(rev_e, boost::tuples::ignore) = boost::add_edge(v, u, G);
-        capacitymap[e] = c;
-        weightmap[e] = w; // new!
-        capacitymap[rev_e] = 0;
-        weightmap[rev_e] = -w; // new
-        revedgemap[e] = rev_e; 
-        revedgemap[rev_e] = e; 
-    }
+	// to use the Function (add an edge)
+	void addEdge(int from, int to, long capacity) {
+		Edge e, rev_e;
+		bool success;
+		boost::tie(e, success) = boost::add_edge(from, to, G);
+		boost::tie(rev_e, success) = boost::add_edge(to, from, G);
+		capacitymap[e] = capacity;
+		capacitymap[rev_e] = 0;
+		revedgemap[e] = rev_e;
+		revedgemap[rev_e] = e;
+	}
 };
 
 void testcases() {
@@ -66,59 +63,23 @@ void testcases() {
 	Graph G(n);
 	int source = 0;
 	int target = n-1;
-    EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
-    EdgeWeightMap weightmap = boost::get(boost::edge_weight, G);
-    ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
-    ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, G);
-    EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+	EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
+	ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
+	ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, G);
+	EdgeAdder eaG(G, capacitymap, revedgemap);
+
 	
 	for (unsigned int i = 0; i < m; i += 1)
 	{
 	    int from, to, cost;
 	    cin >> from >> to >> cost;
 	    
-	    eaG.addEdge(from, to, 1, cost);
+	    eaG.addEdge(from, to, cost);
 	}
     
-    boost::successive_shortest_path_nonnegative_weights(G, source, target);
-    
-    // BFS to find vertex set S
-	std::vector<int> vis(n, false); // visited flags
-	std::queue<int> Q; // BFS queue (from std:: not boost::)
-	vis[source] = true; // Mark the source as visited
-	Q.push(source);
-	while (!Q.empty()) {
-		const int u = Q.front();
-		Q.pop();
-		OutEdgeIt ebeg, eend;
-		for (boost::tie(ebeg, eend) = boost::out_edges(u, G); ebeg != eend; ++ebeg) {
-			const int v = boost::target(*ebeg, G);
-			// Only follow edges with spare capacity
-			if (rescapacitymap[*ebeg] == 0 || vis[v]) continue;
-			vis[v] = true;
-			Q.push(v);
-		}
-	}
-    
-    //go over all vertices of S and their outgoing edges, sum up cost of edges leaving S
-    int total_cost = 0;
-    for (unsigned int i = 0; i < n; i += 1)
-    {
-        if(vis[i]) {
-            //check the outgoing edges of i
-            OutEdgeIt ebeg, eend;
-		    for (boost::tie(ebeg, eend) = boost::out_edges(i, G); ebeg != eend; ++ebeg) {
-			    const int v = boost::target(*ebeg, G);
-			    // Only follow edges with spare capacity
-			    int c = weightmap[*ebeg];
-                if(not vis[v] && c > 0) total_cost += c;
-		    }
-		    
-		    //cout << i << " ";
-        }
-    }
-    //cout << endl;
-    cout << total_cost << endl;
+	int flow = boost::push_relabel_max_flow(G, source, target);
+
+    cout << flow << endl;
 
 }
 
