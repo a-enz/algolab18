07e13a3c412b473c3edfee00e5ac2ba67fbf226e
aenz <aenz@student.ethz.ch>
2018-01-14 16:13:31 +0100

    WiP

---
+++ 12/goldeneye/main.cpp
@@ -0,0 +1,27 @@
+
+// STL includes
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cassert>
+#include "prettyprint.hpp"
+
+// Namespaces
+using namespace std;
+
+// Functions
+// ========= 
+void testcases() {
+
+}
+
+// Main function looping over the testcases
+int main() {
+	ios_base::sync_with_stdio(false);
+	int T;	cin >> T;	// First input line: Number of testcases.
+	while(T--)	testcases();
+	return 0;
+}
+
+
