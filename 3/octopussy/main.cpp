d023f5e5f6fed2300923093a41b9e20eb605b6d9
a-enz <andi_enz@msn.com>
2018-01-16 16:06:43 +0100

    Done

--- 3/octopussy/main.cpp
+++ 3/octopussy/main.cpp
@@ -2,75 +2,58 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
-#include <assert.h>
-#include <map>
+#include <cassert>
 #include <queue>
 
 using namespace std;
-
-typedef map<int, queue<int> >::iterator		timer_it;
 
 void testcase() {
 	int n_bombs;
 	cin >> n_bombs;
 
-	map<int, queue<int> > timers;
-	vector<bool> visited(n_bombs, false);
+	vector<int> timer(n_bombs);
 
-	for(int i=0; i<n_bombs; i++) {
-		int timer;
-		cin >> timer;
+	for (int i = 0; i < n_bombs; ++i)
+	{
+		cin >> timer[i];
+	}
 
-		try{
-			timers.at(timer).push(i);
-		}
-		catch(const out_of_range& oor) {
-			queue<int> q;
-			q.push(i);
-			timers.insert(pair<int, queue<int> >(timer, q));
+	// traverse the tree and keep track of the explosion timers
+	vector<int> order;
+	order.reserve(n_bombs);
+
+	//queue of pairs (timer[i], i)
+	//ensure queue does the right comparison
+	auto comp = [&](const int& i, const int& j) -> bool{
+		return timer[i] < timer[j];
+	};
+
+	priority_queue<int, vector<int>, decltype(comp)> Q(comp);
+
+	Q.push(0);
+
+	while(not Q.empty()) {
+		int j = Q.top();
+		Q.pop();
+		order.push_back(timer[j]);
+
+		if(j <= (n_bombs - 3) / 2) {
+			Q.push(2*j + 1);
+			Q.push(2*j + 2);	
 		}
 	}
 
-	bool exploded = false;
-	timer_it t_it = timers.begin();
-	int time = 1;
-
-	while(t_it->first <= n_bombs) {
-		int current_time = t_it->first;
-		queue<int> q = t_it->second;
-
-		//check if the time is still valid
-		if(current_time < time) {
-			exploded = true;
-			break;
+	int defuse_counter = 1;
+	for (int i = order.size()-1; i >= 0; --i)
+	{
+		if(order[i] < defuse_counter) {
+			cout << "no\n";
+			return;
 		}
-
-		while(not q.empty()) {
-
-			int node = q.pop();
-
-			if(not visited[node]) {
-				//check if children visited if bomb not in bottom level
-				int start_lowest_lvl = n_bombs / 2;
-				if(node > start_lowest_lvl) {
-
-					//if children visited and bomb timer not too early
-					//increment timer and set bomb to visited
-
-					//if children not visited check tree downwards
-				} else { //node in last level
-					time++;
-				}
-			}
-		}
-
-		t_it++;
+		defuse_counter++;
 	}
 
-	if(exploded)
-		cout << "no\n";
-	else
-		cout << "yes\n";
+	cout << "yes\n";
 }
 
 int main(void) {
