043fa4dba0c4170df50994047314b240f7970801
a-enz <andi_enz@msn.com>
2018-01-17 19:51:59 +0100

    Just starting

---
+++ 14/fleetrace/main.cpp
@@ -0,0 +1,35 @@
+// ALGOLAB BGL Tutorial 1
+// Tutorial example problem
+
+// Compile and run with one of the following:
+// g++ -std=c++11 -O2 bgl-tutorial_problem.cpp -o bgl-tutorial_problem; ./bgl-tutorial_problem < bgl-tutorial_problem.in
+// g++ -std=c++11 -O2 -I path/to/boost_1_58_0 bgl-tutorial_problem.cpp -o bgl-tutorial_problem; ./bgl-tutorial_problem < bgl-tutorial_problem.in
+
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cassert>
+#include "prettyprint.hpp"
+
+// Namespaces
+using namespace std;
+
+
+// Functions
+// ========= 
+void testcases() {
+	
+}
+
+// Main function looping over the testcases
+int main() {
+	ios_base::sync_with_stdio(false);
+	int T;	cin >> T;	// First input line: Number of testcases.
+	while(T--)	testcases();
+	return 0;
+}
+
