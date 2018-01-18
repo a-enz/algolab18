c1062f753305990c4334aad0561c91c0fd998fac
a-enz <andi_enz@msn.com>
2018-01-18 14:08:26 +0100

    Done

--- 14/fleetrace/main.cpp
+++ 14/fleetrace/main.cpp
@@ -14,15 +14,114 @@
 #include <climits>
 #include <cassert>
 #include "prettyprint.hpp"
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
 
 // Namespaces
 using namespace std;
 
+// BGL Graph definitions
+// ===================== 
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, Traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > Graph; // new!
+// Interior Property Maps
+typedef boost::property_map<Graph, boost::edge_capacity_t>::type      EdgeCapacityMap;
+typedef boost::property_map<Graph, boost::edge_weight_t >::type       EdgeWeightMap; // new!
+typedef boost::property_map<Graph, boost::edge_residual_capacity_t>::type ResidualCapacityMap;
+typedef boost::property_map<Graph, boost::edge_reverse_t>::type       ReverseEdgeMap;
+typedef boost::graph_traits<Graph>::vertex_descriptor          Vertex;
+typedef boost::graph_traits<Graph>::edge_descriptor            Edge;
+typedef boost::graph_traits<Graph>::out_edge_iterator  OutEdgeIt; // Iterator
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map, weight map and reverse edge map
+// ===============================================================
+class EdgeAdder {
+    Graph &G;
+    EdgeCapacityMap &capacitymap;
+    EdgeWeightMap &weightmap;
+    ReverseEdgeMap  &revedgemap;
+
+public:
+    EdgeAdder(Graph &G, EdgeCapacityMap &capacitymap, EdgeWeightMap &weightmap, ReverseEdgeMap &revedgemap) 
+        : G(G), capacitymap(capacitymap), weightmap(weightmap), revedgemap(revedgemap) {}
+
+    void addEdge(int u, int v, long c, long w) {
+        Edge e, rev_e;
+        boost::tie(e, boost::tuples::ignore) = boost::add_edge(u, v, G);
+        boost::tie(rev_e, boost::tuples::ignore) = boost::add_edge(v, u, G);
+        capacitymap[e] = c;
+        weightmap[e] = w; // new!
+        capacitymap[rev_e] = 0;
+        weightmap[rev_e] = -w; // new
+        revedgemap[e] = rev_e; 
+        revedgemap[rev_e] = e; 
+    }
+};
+
+const int MAX_W = 50;
 
 // Functions
 // ========= 
 void testcases() {
-	
+	size_t b, s, p;
+	cin >> b >> s >> p;
+
+	// Create Graph and Maps
+	size_t V = b+s+3;
+	Vertex source = V-1;
+	Vertex target = V-2;
+	Vertex int_source = V-3;
+	int boat_offset = s;
+    Graph G(V);
+    EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
+    EdgeWeightMap weightmap = boost::get(boost::edge_weight, G);
+    ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
+    ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, G);
+    EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+    
+    //read and add the s->b edges
+	for (int i = 0; i < p; ++i)
+	{	
+		int to, from, c;
+		cin >> to >> from >> c;
+		eaG.addEdge(from, to + boat_offset, 1, MAX_W - c);
+		//cout << from << " " << to << " " << c << endl;
+	}
+
+	//connect int_source to all sailors
+	for (int i = 0; i < s; ++i)
+	{
+		eaG.addEdge(int_source, i, 1, 0);
+	}
+
+	//connect boats to target
+	for (int i = 0; i < b; ++i)
+	{
+		eaG.addEdge(i + boat_offset, target, 1, 0);
+	}
+
+	//special edges
+	eaG.addEdge(source, int_source, min(s, b), 0); //restrict flow size
+	//reroute flow that does not find a edge in the s, b pairing
+	eaG.addEdge(int_source, target, min(s, b), MAX_W);
+
+	//compute flow 
+	boost::successive_shortest_path_nonnegative_weights(G, source, target);
+    int cost = boost::find_flow_cost(G);
+    //cout << "cost: " << cost << endl;
+    //compute real cost
+    cost = -1 * (cost - min(s, b) * MAX_W);
+    cout << cost << endl;
 }
 
 // Main function looping over the testcases
