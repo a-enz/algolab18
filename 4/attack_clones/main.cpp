5ca46a48a76fa876c22933a5fa787b5501010a61
aenz <aenz@student.ethz.ch>
2018-01-22 16:21:31 +0100

    Sort of working

--- 4/attack_clones/main.cpp
+++ 4/attack_clones/main.cpp
@@ -10,9 +10,7 @@
 
 using namespace std;
 
-struct Interval{
-    int a, b;
-};
+typedef pair<int, int> Interval;
 
 void testcase() {
 	int n_jedi, m_seg;
@@ -33,7 +31,7 @@ void testcase() {
         //after converting to 0 indexing
         assert(b < 2*m_seg - 1);
 
-        segment[i] = {a, b}; //convert to zero indexing
+        segment[i] = make_pair(a, b); //convert to zero indexing
     }
 
     //create a map: seg -> (#starting, #ending)
@@ -42,8 +40,8 @@ void testcase() {
 
     for (int i = 0; i < n_jedi; ++i)
     {
-        int start = segment[i].a;
-        int end = segment[i].b;
+        int start = segment[i].first;
+        int end = segment[i].second;
         segment_starts[start]++;
         segment_ends[end+1]++; // interval is inclusive, thus end is one after
     }
@@ -79,48 +77,62 @@ void testcase() {
 
     //shift all segments
     vector<Interval> shifted_segments = segment;
+    
     for (int i = 0; i < n_jedi; ++i)
     {
-        shifted_segments[i].a = (shifted_segments[i].a - (shift_idx+1)) % (2 * m_seg);
-        shifted_segments[i].b = (shifted_segments[i].b - (shift_idx+1)) % (2 * m_seg);
-/*
+        shifted_segments[i].first = (shifted_segments[i].first - (shift_idx+1)) % (2 * m_seg);
+        shifted_segments[i].second = (shifted_segments[i].second - (shift_idx+1)) % (2 * m_seg);
+
         //ensure no negative modulo
-        if(shifted_segments[i].a < 0)
-            shifted_segments[i].a += 2*m_seg;
-        if(shifted_segments[i].b < 0)
-            shifted_segments[i].b += 2*m_seg;*/
+        if(shifted_segments[i].first < 0)
+            shifted_segments[i].first += 2*m_seg;
+        if(shifted_segments[i].second < 0)
+            shifted_segments[i].second += 2*m_seg;
     }
+            
 
 /*
     cout << covered << endl;
     cout << "shift " << shift_idx+1 << endl;
     for (int i = 0; i < n_jedi; ++i)
     {  
-        cout << "shift " << shifted_segments[i].a << " shift " << shifted_segments[i].b << endl;
-        cout << "start " << segment[i].a << " ende " << segment[i].b << endl;
+        cout << "shift " << shifted_segments[i].first << " shift " << shifted_segments[i].second << endl;
+        cout << "start " << segment[i].first << " ende " << segment[i].second << endl;
     }
 */
 
     //do EDF on shifted
     sort(shifted_segments.begin(), shifted_segments.end(), 
         [](const Interval& i1, const Interval& i2) -> bool {
-            if(i1.b == i2.b)
-                return i1.a < i2.a;
+            if(i1.second == i2.second)
+                return i1.first < i2.first;
             else
-                return i1.b < i2.b;
+                return i1.second < i2.second;
         });
+        
+    //cerr << "segments " << segment << endl;
+    //cerr << "shifted segments "  << shifted_segments << endl;
+    
 
     int jcounter = 1;
+    bool stop = false;
     Interval prev = shifted_segments[0];
+    //cerr << "picking " << prev.first << ", " << prev.second << endl;
     for (int i = 1; i < n_jedi; ++i)
     {   
         //while the next 'a' (start)
         //id before or equal to previous 'b' (end)
         //discard
-        while(prev.b >= shifted_segments[i].a)
+        while(prev.second % m_seg >= shifted_segments[i].first % m_seg) {
+            if(i > n_jedi-1) {
+                stop = true;
+                break;
+            }
             i++;
-
+        }
+        if(stop) break;
         prev = shifted_segments[i];
+        //cerr << "picking " << prev.first << ", " << prev.second << endl;
         jcounter++;
     }
 
