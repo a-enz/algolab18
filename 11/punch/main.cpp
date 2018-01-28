7d92b86fb72ab3f853c29d831ccfe27397f9d33a
aenz <aenz@student.ethz.ch>
2018-01-28 12:32:58 +0100

    In progress

--- 11/punch/main.cpp
+++ 11/punch/main.cpp
@@ -5,8 +5,16 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <tuple>
+#include <cassert>
+#include <cmath>
+#include "prettyprint.hpp"
+#include <unordered_map>
+#include <boost/functional/hash.hpp>
 
 using namespace std;
+
+typedef tuple<int, int> Tuple_2;
 
 struct Bev {
     int cost;
@@ -14,9 +22,71 @@ struct Bev {
     int costprio;
 };
 
+unordered_map< Tuple_2,Tuple_2, boost::hash<Tuple_2 > > memo;
+
+Tuple_2 min_cost_max_bev(int pos, int liters, vector<Bev>& bev) {
+    Tuple_2 res = make_tuple(1000000, 0);
+     
+    Tuple_2 fvalue = make_tuple(pos, liters);
+    if(memo.find(fvalue) != memo.end()) 
+        return memo[fvalue];
+    
+    assert(pos >= 0);
+    if(pos == bev.size() || liters <= 0) {
+        memo[fvalue] = res;
+        return memo[fvalue];
+    }
+
+
+    auto comp = [](const Tuple_2& a, const Tuple_2& b)->bool{
+                    int c_a, c_b, b_a, b_b;
+                    tie(c_a, b_a) = a;
+                    tie(c_b, b_b) = b;
+                    
+                    if(c_a == c_b)
+                        return b_a > b_b;
+                    else
+                        return c_a < c_b;
+                };
+                
+    //take beverage 0 times
+    res = min(res, min_cost_max_bev(pos+1, liters, bev), comp);
+    
+//    //take it multiple times
+//    int a = 1;
+//    for (; a * bev[pos].vol < liters; a += 1)
+//    {
+//       int cost, d_bev;
+//       tie(cost, d_bev) = min_cost_max_bev(pos+1, liters - a * bev[pos].vol, bev);
+//       res = min(res, make_tuple(cost + bev[pos].cost * a, d_bev + 1), comp); 
+//       //cout << "updating res " << res << endl;
+//    }
+    
+    //take beverage 1 time
+    int cost, d_bev;
+    tie(cost, d_bev) = min_cost_max_bev(pos+1, liters - bev[pos].vol, bev);
+    res = min(res, , comp);
+    
+    
+    
+    
+    
+    //take it so many times we fill up all the remaining liters
+    int amount = 
+    res = min(res, make_tuple( * bev[pos].cost, 1), comp); 
+    
+//    cout << "returning " << res << endl;
+    memo[fvalue] = res;
+    return memo[fvalue];
+}
+
+
 void testcases() {
     size_t n_bev, k_ppl;
     cin >> n_bev >> k_ppl;
+    
+    //clear memo;
+    memo.clear();
     
     vector<Bev> beverage(n_bev);
     int min_costprio = INT_MAX;
@@ -36,21 +106,10 @@ void testcases() {
                         beverage[i].costprio : min_costprio;
     }
     
-    sort(beverage.begin(), beverage.end(), [](const Bev& a, const Bev& b) -> bool {
-        if(a.costprio == b.costprio)
-            return a.vol < b.vol;
-        else
-            return a.costprio < b.costprio;
-    });
+    int min_cost, max_bev;
+    tie(min_cost, max_bev) = min_cost_max_bev(0, k_ppl, beverage);
     
-    int total_vol = 0, b = 0;
-    while(beverage[b].costprio == min_costprio && total_vol < k_ppl) {
-        total_vol += beverage[b].vol;
-        b++;
-    }
-    
-    cout << beverage[0].costprio << " " << b << endl;
-    
+    cout << min_cost << " " << max_bev << endl;
 }
 
 // Main function looping over the testcases
