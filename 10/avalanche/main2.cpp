c320a4ce4121bacbe9a6085e40841da0b4b7d566
a-enz <andi_enz@msn.com>
2018-01-30 21:59:39 +0100

    Take2

--- 10/avalanche/main2.cpp
+++ 10/avalanche/main2.cpp
@@ -6,13 +6,10 @@
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/max_cardinality_matching.hpp>
 
-// Namespaces
 using namespace std;
 
-// BGL Graph definitions
-// =====================
-// Graph Type, OutEdgeList Type, VertexList Type, (un)directedS
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,		// Use vecS for the VertexList! Choosing setS for the OutEdgeList disallows parallel edges.
 		boost::no_property,				// interior properties of vertices	
 		boost::property<boost::edge_weight_t, int> 		// interior properties of edges
@@ -23,8 +20,43 @@ typedef boost::graph_traits<Graph>::edge_iterator		EdgeIt;		// to iterate over a
 typedef boost::graph_traits<Graph>::out_edge_iterator		OutEdgeIt;	// to iterate over all outgoing edges of a vertex
 typedef boost::property_map<Graph, boost::edge_weight_t>::type	WeightMap;	// property map to access the interior property edge_weight_t
 
+//undir graph for the matching
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,     // Use vecS for the VertexList! Choosing setS for the OutEdgeList disallows parallel edges.
+        boost::no_property,             // interior properties of vertices  
+        boost::no_property      // interior properties of edges
+        >                   UndirGraph;
+typedef boost::graph_traits<UndirGraph>::vertex_descriptor       UVertex;     // Vertex Descriptor: with vecS vertex list, this is really just an int in the range [0, num_vertices(G)).  
+
 // Functions
 // ========= 
+bool too_small(int time, int d, int cap, const vector< vector<int> >& asmap) {
+    //build bipartite graph with edges below given time
+    //  'edge time' + 'enter delay d' == 'time'
+    int a_agents = asmap.size();
+    int s_shelters = asmap[0].size();
+    int V = a_agents + cap * s_shelters;
+    UndirGraph G(V);
+    for (int a = 0; a < a_agents; ++a)
+    {
+        for (int s = 0; s < s_shelters; ++s)
+        {   
+            for (int c = 1; c <= cap; ++c)
+            {
+                if(asmap[a][s] <= time - c * d)
+                    boost::add_edge(a, a_agents + s + s_shelters * (c-1), G);
+            }
+        }
+    }
+
+    //compute matching
+    vector<UVertex> matemap(V);     // We MUST use this vector as an Exterior Property Map: Vertex -> Mate in the matching
+    boost::edmonds_maximum_cardinality_matching(G, boost::make_iterator_property_map(matemap.begin(), get(boost::vertex_index, G)));
+    int matchingsize = matching_size(G, boost::make_iterator_property_map(matemap.begin(), get(boost::vertex_index, G)));
+
+    return matchingsize < a_agents;
+}
+
+
 void testcases() {
     int n_pos, m_slopes, a_agents, s_shelters, c_cap, d_time;
     cin >> n_pos >> m_slopes >> a_agents >> s_shelters >> c_cap >> d_time;
@@ -35,9 +67,8 @@ void testcases() {
 	
 	for (unsigned int i = 0; i < m_slopes; i += 1)
 	{
-	    char w;
-	    int x, y, z;
-	    cin >> w >> x >> y >> z
+	    char w; int x, y, z;
+	    cin >> w >> x >> y >> z;
 	    Edge e;
 		boost::tie(e, std::ignore) = boost::add_edge(x, y, G);
 		weightmap[e] = z;
@@ -51,10 +82,40 @@ void testcases() {
 	
 	vector< vector<int> > agent_distmap(a_agents, vector<int>(V));
 	for (unsigned int i = 0; i < a_agents; i += 1)
-	{
-	    
+	{  
+        int start;
+        cin >> start; //start of agent i
+	    boost::dijkstra_shortest_paths(G, start, // We MUST provide at least one of the two maps
+            boost::distance_map(boost::make_iterator_property_map(agent_distmap[i].begin(), boost::get(boost::vertex_index, G))));  // distance
 	}
-	
+
+    vector< vector<int> > asmap(a_agents, vector<int>(s_shelters));
+    for (int i = 0; i < s_shelters; ++i)
+    {
+        int pos;
+        cin >> pos;
+        for (int a = 0; a < a_agents; ++a)
+            asmap[a][i] = agent_distmap[a][pos];
+    }
+
+    //now do binary search, checking if every agent
+    //can be matched with a shelter given the bomb timer
+    int lmin = 1, lmax = 1;
+    while(2*lmax <= INT_MAX && too_small(lmax, d_time, c_cap, asmap)) {
+        lmin = lmax;
+        lmax *= 2;
+    }
+
+    while(lmin < lmax) {
+        int mid = lmin + (lmax - lmin) / 2;
+
+        if(too_small(mid, d_time, c_cap, asmap))
+            lmin = mid+1;
+        else
+            lmax =   mid;
+    }
+
+    cout << lmin << endl;
 }
 
 // Main function looping over the testcases
