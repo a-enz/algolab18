1c63e99c82d9ed84e5bd8232ca4c1eb1bea77706
a-enz <andi_enz@msn.com>
2018-02-04 11:24:37 +0100

    Better?

--- algo16/DHL/main.cpp
+++ algo16/DHL/main.cpp
@@ -17,34 +17,54 @@ typedef vector< vector< vector< vector<int> > > > Vmap;
 vector<int> b_ps, a_ps;
 Vmap memo;
 
-int min_cost(int i1, int i2, int k1, int k2) {
+int min_cost(int a, int i, int k1, int k2) {
     //check memo
-    if(memo[i1][i2][k1][k2] != -1)
-    return memo[i1][i2][k1][k2];
+    if(memo[a][i][k1][k2] != -1)
+    return memo[a][i][k1][k2];
 
-    if(i2 == k2 && i2+1 == a_ps.size()) {
-        int result = (a_ps[i2] - a_ps[i1-1] - (i2-i1 + 1)) * 
+    if(i == k2 && i+1 == a_ps.size()) {
+        int val;
+        if(a==1) {
+            val = (a_ps[i] - a_ps[i-1] - 1) * 
                 (b_ps[k2] - b_ps[k1-1] - (k2-k1 + 1));
-        memo[i1][i2][k1][k2] = result;
-        return memo[i1][i2][k1][k2];
+        }
+        else {
+            assert(a==0);
+            val = (b_ps[i] - b_ps[i-1] - 1) * 
+                (a_ps[k2] - a_ps[k1-1] - (k2-k1 + 1));
+        }
+
+        memo[a][i][k1][k2] = val;
+        return memo[a][i][k1][k2];
     }
-    else if(i2 != k2 && (i2 == a_ps.size() || k2 == a_ps.size())) {
-        memo[i1][i2][k1][k2] = pow(10, 9); //basically inf
-        return memo[i1][i2][k1][k2];
+    else if(i != k2 && (i == a_ps.size() || k2 == a_ps.size())) {
+        memo[a][i][k1][k2] = pow(10, 9); //basically inf
+        return memo[a][i][k1][k2];
     }
 
 
     //general case
-    int opt1 = (a_ps[i2] - a_ps[i1-1] - (i2-i1 + 1)) * 
-                (b_ps[k2] - b_ps[k1-1] - (k2-k1 + 1)) 
-                + min_cost(i2+1, i2+1, k2+1, k2+1);
-    int opt2 = min_cost(i1, i2+1, k1, k2+1);
-    int opt3 = min_cost(i1, i2+1, k1, k2);
-    int opt4 = min_cost(i1, i2, k1, k2+1);
+    int opt1, opt2;
+    if(a==1) {
+        int val = (a_ps[i] - a_ps[i-1] - 1) * 
+                (b_ps[k2] - b_ps[k1-1] - (k2-k1 + 1));
+        opt1 = val + min_cost(a, i+1, k2+1, k2+1);
+        opt2 = val + min_cost(1-a, k2+1, i+1, i+1);
+    }
+    else {
+        assert(a==0);
+        int val = (b_ps[i] - b_ps[i-1] - 1) * 
+                (a_ps[k2] - a_ps[k1-1] - (k2-k1 + 1));
+
+        opt1 = val + min_cost(a, i+1, k2+1, k2+1);
+        opt2 = val + min_cost(1-a, k2+1, i+1, i+1);
+    }
+
+    int opt3 = min_cost(a, i, k1, k2+1);
 
 /*    cout << "options " << opt1 << " " << opt2 << " " << opt3 << " " << opt4 << endl;
-*/   memo[i1][i2][k1][k2] = min({opt1, opt2, opt3, opt4});
-    return memo[i1][i2][k1][k2];
+*/   memo[a][i][k1][k2] = min({opt1, opt2, opt3});
+    return memo[a][i][k1][k2];
 }
 
 
